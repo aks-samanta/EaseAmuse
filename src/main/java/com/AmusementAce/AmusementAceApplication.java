dae3ed63c91cf430204266b43ea3c236b6aeed65
Akash Samanta <101733074+aks-samanta@users.noreply.github.com>
2023-01-16 23:02:50 +0530

    structure ready, dependencies + properties added

---
+++ src/main/java/com/AmusementAce/AmusementAceApplication.java
@@ -0,0 +1,13 @@
+package com.AmusementAce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmusementAceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmusementAceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AmusementAce/AmusementAceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AmusementAce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmusementAceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
