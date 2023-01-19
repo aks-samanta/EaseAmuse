9c4301b4c0bb27ffba5ee6bcc98d313cb0d32e71
Akash Samanta <101733074+aks-samanta@users.noreply.github.com>
2023-01-19 12:53:02 +0530

    exception handlers added

---
+++ src/main/java/com/EaseAmuse/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,44 @@
+package com.EaseAmuse.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	String resourceName;
+	String fieldName;
+	long fieldValue;
+
+	public ResourceNotFoundException() {
+
+	}
+
+	public ResourceNotFoundException(String resourceName, String feildName, long fieldValue) {
+		super(String.format("%s not found with %s : %s", resourceName, feildName, fieldValue));
+		this.resourceName = resourceName;
+		this.fieldName = feildName;
+		this.fieldValue = fieldValue;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+
+	public String getFeildName() {
+		return fieldName;
+	}
+
+	public void setFeildName(String feildName) {
+		this.fieldName = feildName;
+	}
+
+	public long getFieldValue() {
+		return fieldValue;
+	}
+
+	public void setFieldValue(long fieldValue) {
+		this.fieldValue = fieldValue;
+	}
+
+}
