7aca981a2d2030f6c8cd759254307ab209afb00a
chiranjivi pattanayak <103636168+ChiranjiviPattanayak@users.noreply.github.com>
2023-01-22 20:56:23 +0530

    Done Manager Controller

--- src/main/java/com/EaseAmuse/services/ManagerServicesImpl.java
+++ src/main/java/com/EaseAmuse/services/ManagerServicesImpl.java
@@ -12,16 +12,20 @@ import org.springframework.stereotype.Service;
 
 import com.EaseAmuse.exceptions.CustomerException;
 import com.EaseAmuse.exceptions.ResourceNotFoundException;
+import com.EaseAmuse.models.Activity;
 import com.EaseAmuse.models.AmusementPark;
 import com.EaseAmuse.models.Customer;
 import com.EaseAmuse.models.DailyActivity;
 import com.EaseAmuse.models.Manager;
+import com.EaseAmuse.payloads.ActivityInputDto;
+import com.EaseAmuse.payloads.ActivityOutputDto;
 import com.EaseAmuse.payloads.AmusementParkInputDto;
 import com.EaseAmuse.payloads.AmusementParkOutputDto;
 import com.EaseAmuse.payloads.CustomerOutputDto;
 import com.EaseAmuse.payloads.DailyActivityOutputDto;
 import com.EaseAmuse.payloads.ManagerInputDto;
 import com.EaseAmuse.payloads.ManagerOutputDto;
+import com.EaseAmuse.repositories.ActivityRepo;
 import com.EaseAmuse.repositories.AmusementParkRepo;
 import com.EaseAmuse.repositories.CustomerRepo;
 import com.EaseAmuse.repositories.DailyActivityRepo;
@@ -43,7 +47,13 @@ public class ManagerServicesImpl implements ManagerServices {
 	AmusementParkServices amusementParkServices;
 
 	@Autowired
+	AmusementParkRepo parkRepo;
+
+	@Autowired
 	CustomerRepo customerRepo;
+
+	@Autowired
+	ActivityRepo activityRepo;
 
 	@Override
 	public ManagerOutputDto insertManager(ManagerInputDto managerInpDto) throws ResourceNotFoundException {
@@ -121,7 +131,8 @@ public class ManagerServicesImpl implements ManagerServices {
 	}
 
 	@Override
-	public List<DailyActivityOutputDto> getDailyActivitiesDatewise(Integer managerId, Date activityDate) throws ResourceNotFoundException {
+	public List<DailyActivityOutputDto> getDailyActivitiesDatewise(Integer managerId, Date activityDate)
+			throws ResourceNotFoundException {
 
 		Manager manager = this.managerRepo.findById(managerId)
 				.orElseThrow(() -> new ResourceNotFoundException("Manager", "Manager Id", managerId.toString()));
@@ -131,6 +142,45 @@ public class ManagerServicesImpl implements ManagerServices {
 
 		return activities.stream().map((da) -> this.modelMapper.map(da, DailyActivityOutputDto.class))
 				.collect(Collectors.toList());
+
+	}
+
+	@Override
+	public List<ActivityOutputDto> getAllActivities(Integer managerId) throws ResourceNotFoundException {
+		Manager manager = this.managerRepo.findById(managerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Manager", "Manager Id", managerId.toString()));
+
+		List<Activity> activities = manager.getAmusementPark().getActivities();
+
+		return activities.stream().map((a) -> this.modelMapper.map(a, ActivityOutputDto.class))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public ActivityOutputDto createActivity(Integer managerId, ActivityInputDto activityDto) {
+
+		Manager manager = this.managerRepo.findById(managerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Manager", "Manager Id", managerId.toString()));
+
+		AmusementPark park = manager.getAmusementPark();
+
+		Activity activity = this.modelMapper.map(activityDto, Activity.class);
+		activity.setAmusementPark(park);
+		park.getActivities().add(activity);
+
+		AmusementPark updatedPark = this.parkRepo.save(park);
+
+		return this.modelMapper.map(updatedPark.getActivities().get((updatedPark.getActivities().size() - 1)),
+				ActivityOutputDto.class);
+	}
+
+	@Override
+	public AmusementParkOutputDto getAmusementPark(Integer managerId) {
+
+		Manager manager = this.managerRepo.findById(managerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Manager", "Manager Id", managerId.toString()));
+
+		return this.modelMapper.map(manager.getAmusementPark(), AmusementParkOutputDto.class);
 
 	}
 
