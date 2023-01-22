b1404ed3b35eb56e5fb44c2047d236a5de2c8dcd
Akash Samanta <101733074+aks-samanta@users.noreply.github.com>
2023-01-22 17:18:19 +0530

    book ticket cancel ticket completed

--- src/main/java/com/EaseAmuse/services/TicketServicesImpl.java
+++ src/main/java/com/EaseAmuse/services/TicketServicesImpl.java
@@ -1,20 +1,78 @@
 package com.EaseAmuse.services;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.EaseAmuse.exceptions.ResourceNotFoundException;
+import com.EaseAmuse.exceptions.UnauthorisedException;
+import com.EaseAmuse.models.Customer;
+import com.EaseAmuse.models.DailyActivity;
 import com.EaseAmuse.models.Ticket;
+import com.EaseAmuse.models.TicketStatus;
+import com.EaseAmuse.payloads.TicketInputDto;
+import com.EaseAmuse.payloads.TicketOutputDto;
+import com.EaseAmuse.repositories.CustomerRepo;
+import com.EaseAmuse.repositories.DailyActivityRepo;
+import com.EaseAmuse.repositories.TicketRepo;
 
 public class TicketServicesImpl implements TicketServices {
 
+	@Autowired
+	private DailyActivityRepo dailyActivityRepo;
+
+	@Autowired
+	private CustomerRepo customerRepo;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	@Autowired
+	private TicketRepo ticketRepo;
+
 	@Override
-	public Ticket cancelTicket(Integer ticketId) throws ResourceNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public TicketOutputDto cancelTicket(Integer customerId, Integer ticketId) throws ResourceNotFoundException {
+
+		Ticket foundTicket = this.ticketRepo.findById(ticketId)
+				.orElseThrow(() -> new ResourceNotFoundException("Ticket", "Ticket ID", ticketId.toString()));
+		
+		if(foundTicket.getCustomer().getCustomerId() == customerId) {
+			foundTicket.setTicketStatus(TicketStatus.CANCELLED);
+
+		}
+		else {
+			throw new UnauthorisedException("You are only authorosed to cancel your tickets !");
+		}
+		
+		this.dailyActivityRepo.save(foundTicket.getDailyActivity());
+
+		return this.modelMapper.map(foundTicket, TicketOutputDto.class);
 	}
 
 	@Override
-	public Ticket cancelTicket(Ticket ticket) throws ResourceNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public TicketOutputDto createTicket(Integer customerId, TicketInputDto ticketDto) throws ResourceNotFoundException {
+
+		DailyActivity dailyActivity = dailyActivityRepo.findById(ticketDto.getDailyActivityId())
+				.orElseThrow(() -> new ResourceNotFoundException("DailyActivity", "DailyActivity ID",
+						ticketDto.getDailyActivityId().toString()));
+
+		Customer customer = this.customerRepo.findById(customerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer", "customer ID", customerId.toString()));
+
+		Ticket newTicket = new Ticket();
+		newTicket.setAmount(dailyActivity.getActivity().getCharges() * ticketDto.getNoOfPerson());
+		newTicket.setDailyActivity(dailyActivity);
+		newTicket.setNoOfPerson(ticketDto.getNoOfPerson());
+		newTicket.setTicketStatus(TicketStatus.PENDING);
+		newTicket.setCustomer(customer);
+
+		dailyActivity.getTickets().add(newTicket);
+
+		DailyActivity updateDailyActivity = this.dailyActivityRepo.save(dailyActivity);
+
+		Ticket savedTicket = updateDailyActivity.getTickets().get(updateDailyActivity.getTickets().size() - 1);
+
+		return this.modelMapper.map(savedTicket, TicketOutputDto.class);
+
 	}
 
 }
